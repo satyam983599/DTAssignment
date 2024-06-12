8e73c2df4476171b62e123534d441b4ffbe18b4c
Madhavkumar-04 <madhavkumarchas@gmail.com>
2024-06-12 21:01:42 +0530

    Initial commit

---
+++ Task2/index.js
@@ -0,0 +1,29 @@
+const handlebutton = () => {
+  let sidemenu = document.getElementById("sidemenu");
+  let hiddenJourney = document.getElementById("hiddenJourney");
+  let hiddenicon = document.getElementById("hiddenicon");
+  sidemenu.classList.replace("sidemenuToggle", "sidemenu");
+  hiddenicon.style.display = "none";
+};
+const crossbutton = () => {
+  let sidemenu = document.getElementById("sidemenu");
+  let hiddenicon = document.getElementById("hiddenicon");
+  sidemenu.classList.replace("sidemenu", "sidemenuToggle");
+  hiddenicon.style.display = "block";
+};
+let asset_description = document.getElementsByClassName("span1");
+asset_description = Array.from(asset_description);
+let title = document.getElementsByClassName("titlehead");
+jsonData.tasks[0].assets.forEach((asset, idx) => {
+  title[idx].innerHTML = asset.asset_title + title[idx].innerHTML;
+  asset_description[idx].innerHTML = asset.asset_description;
+  if (idx == 0) {
+    let embed = document.getElementById("embedDiv");
+    embed.innerHTML = `<iframe width="560" height="315" src="${asset.asset_content}" frameborder="0" allowfullscreen></iframe>`;
+  }
+});
+let heading = document.getElementById("title");
+heading.innerHTML = jsonData.tasks[0].task_title;
+let description122 = document.getElementById("description122");
+description122.innerHTML = jsonData.tasks[0].task_description;
+// </script>
